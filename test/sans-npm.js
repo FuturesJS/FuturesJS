da66cf0528a79b53966f4a5c9519c97897f93a80
AJ ONeal <coolaj86@gmail.com>
2010-12-29 20:22:13 -0700

    actually use a feature aside from the require

--- test/sans-npm.js
+++ test/sans-npm.js
@@ -1,1 +1,10 @@
-console.log(require(__dirname + '/../../futures/lib/promise'));
+(function () {
+  "use strict";
+
+  var Futures = require(__dirname + '/../../futures/lib/promise'),
+    promise = Futures.promise();
+
+  promise.fulfill(undefined, "Hello World");
+
+  promise.when(console.log);
+}());
