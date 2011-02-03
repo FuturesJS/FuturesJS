bb1a30f4f4bd44900f4e91e384d4496462c71705
AJ ONeal <coolaj86@gmail.com>
2011-02-03 15:18:51 -0700

    added forEachAsync as optional prototype extension

--- examples/Array.prototype.forEachAsync.js
+++ examples/Array.prototype.forEachAsync.js
@@ -1,28 +1,8 @@
-// Extending Array.prototype.forEachAsync
-(function () {
-  "use strict";
-
-  var Sequence = require('futures/sequence');
-
-  Array.prototype.forEachAsync = function (callback) {
-    var self = this,
-      sequence = Sequence();
-
-    function handleItem(item, i, arr) {
-      sequence.then(function (next) {
-        callback(next, item, i, arr);
-      });
-    }
-
-    this.forEach(handleItem);
-
-    return sequence;
-  };
-}());
-
 // Testing functionality
 (function () {
   "use strict";
+
+  require('futures/forEachAsync');
 
   var count = 0,
     timers = [
