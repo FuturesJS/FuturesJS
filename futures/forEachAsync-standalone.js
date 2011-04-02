882cb0e78af86972e13a76ecf736a3c825065ebc
Jameson Little <t.jameson.little@gmail.com>
2011-04-02 15:27:18 -0600

    Created generic forEachAsync that doesn't override Array's prototype

---
+++ futures/forEachAsync-standalone.js
@@ -0,0 +1,26 @@
+// Extending Array.prototype.forEachAsync
+var __dirname = __dirname || '',
+    provide = provide || function () {};
+(function () {
+  "use strict";
+
+  var Sequence = require((__dirname ? __dirname + '/' : 'futures') + '/sequence');
+
+  function forEachAsync (arr, callback) {
+	var sequence = Sequence();
+
+    function handleItem(item, i, arr) {
+      sequence.then(function (next) {
+        callback(next, item, i, arr);
+      });
+    }
+
+    arr.forEach(handleItem);
+
+	return sequence;
+  }
+
+  module.exports = forEachAsync;
+
+  provide('forEachAsync', module.exports);
+}());
--- futures/forEachAsync.js
+++ futures/forEachAsync.js
@@ -3,20 +3,11 @@ var __dirname = __dirname || '';
 (function () {
   "use strict";
 
-  var Sequence = require((__dirname ? __dirname + '/' : 'futures') + '/sequence');
+  var forEachAsync = require('./forEachAsync-standalone');
+
+  console.log(forEachAsync);
 
   Array.prototype.forEachAsync = function (callback) {
-    var self = this,
-      sequence = Sequence();
-
-    function handleItem(item, i, arr) {
-      sequence.then(function (next) {
-        callback(next, item, i, arr);
-      });
-    }
-
-    this.forEach(handleItem);
-
-    return sequence;
+    return forEachAsync(this, callback);
   };
 }());
