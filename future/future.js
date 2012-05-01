b8c2ce84f2e2002c6696c2b7ff92e4d8e7c0aaf3
AJ ONeal <coolaj86@gmail.com>
2012-05-01 17:06:55 -0600

    changed to prototype, tested, and jshinted

--- future/future.js
+++ future/future.js
@@ -1,69 +1,61 @@
+/*jshint laxcomma:true node:true es5:true onevar:true */
 (function () {
   "use strict";
 
   var MAX_INT = Math.pow(2,52);
 
   function isFuture(obj) {
-    return obj instanceof future;
+    return obj instanceof Future;
   }
 
-  function futureTimeout(time) {
+  function FutureTimeoutException(time) {
     this.name = "FutureTimeout";
     this.message = "timeout " + time + "ms";
   }
 
+  //
+  function privatize(obj, pubs) {
+    var result = {};
+    pubs.forEach(function (pub) {
+      result[pub] = function () {
+        obj[pub].apply(obj, arguments);
+        return result;
+      };
+    });
+    return result;
+  }
 
-
-  function future(global_context, options) {
-    var everytimers = {},
-      onetimers = {},
-      index = 0,
-      deliveries = 0,
-      time = 0,
-      fulfilled,
-      data,
-      timeout_id,
-      //asap = false,
-      asap =  true,
-      passenger,
-      self = this;
-
-    // TODO change `null` to `this`
-    global_context = ('undefined' === typeof global_context ? null : global_context);
-
-    options = options || {};
-    options.error = options.error || function (err) {
-      throw err;
-    };
-
-    function resetTimeout() {
-      if (timeout_id) {
-        clearTimeout(timeout_id);
-        timeout_id = undefined;
-      }
-
-      if (time > 0) {
-        timeout_id = setTimeout(function () {
-          self.deliver(new futureTimeout(time));
-          timeout_id = undefined;
-        }, time);
-      }
+  function Future(global_context, options) {
+    if (!this || this === global) {
+      return new Future(global_context, options);
     }
 
+    var self = this
+      ;
+      
+    self._everytimers = {};
+    self._onetimers = {};
+    self._index = 0;
+    self._deliveries = 0;
+    self._time = 0;
+    //self._asap = false;
+    self._asap =  true;
 
+    //self._data;
+    //self._timeout_id;
 
-    self.isFuture = isFuture;
+    self._passenger = null;
+    self.fulfilled = false;
 
-    self.setContext = function (context) {
-      global_context = context;
+    self._global_context = global_context;
+
+    // TODO change `null` to `this`
+    self._global_context = ('undefined' === typeof self._global_context ? null : self._global_context);
+
+    self._options = options || {};
+    self._options.error = self._options.error || function (err) {
+      throw err;
     };
-
-    self.setTimeout = function (new_time) {
-      time = new_time;
-      resetTimeout();
-    };
-
-
 
     self.errback = function () {
       if (arguments.length < 2) {
@@ -73,8 +65,6 @@
       }
     };
 
-
-
     self.callback = function () {
       var args = Array.prototype.slice.call(arguments);
 
@@ -82,108 +72,80 @@
       self.deliver.apply(self, args);
     };
 
-
-
-    self.callbackCount = function() {
-      return Object.keys(everytimers).length;
-    };
-
-
-
-    self.deliveryCount = function() {
-      return deliveries;
-    };
-
-
-
-    self.setAsap = function(new_asap) {
-      if (undefined === new_asap) {
-        new_asap = true;
+    self.fulfill = function () {
+      if (arguments.length) {
+        self.deliver.apply(self, arguments);
+      } else {
+        self.deliver();
       }
-      if (true !== new_asap && false !== new_asap) {
-        options.error(new Error("Future.setAsap(asap) accepts literal true or false, not " + new_asap));
+      self.fulfilled = true;
+    };
+
+    self.when = function (callback, local_context) {
+      // this self._index will be the id of the everytimer
+      self._onetimers[self._index] = true;
+      self.whenever(callback, local_context);
+
+      return self;
+    };
+
+    self.whenever = function (callback, local_context) {
+      var id = self._index,
+        everytimer;
+
+      if ('function' !== typeof callback) {
+        self._options.error(new Error("Future().whenever(callback, [context]): callback must be a function."));
         return;
       }
-      asap = new_asap;
-    };
 
+      if (self._findCallback(callback, local_context)) {
+        // TODO log
+        self._options.error(new Error("Future().everytimers is a strict set. Cannot add already subscribed `callback, [context]`."));
+        return;
+      }
 
+      everytimer = self._everytimers[id] = {
+        id: id,
+        callback: callback,
+        context: (null === local_context) ? null : (local_context || self._global_context)
+      };
 
-    // this will probably never get called and, hence, is not yet well tested
-    function cleanup() {
-      var new_everytimers = {},
-        new_onetimers = {};
-
-      index = 0;
-      Object.keys(everytimers).forEach(function (id) {
-        var newtimer = new_everytimers[index] = everytimers[id];
-
-        if (onetimers[id]) {
-          new_onetimers[index] = true;
+      if (self._asap && self._deliveries > 0) {
+        // doesn't raise deliver count on purpose
+        everytimer.callback.apply(everytimer.context, self._data);
+        if (self._onetimers[id]) {
+          delete self._onetimers[id];
+          delete self._everytimers[id];
         }
+      }
 
-        newtimer.id = index;
-        index += 1;
-      });
-
-      onetimers = new_onetimers;
-      everytimers = new_everytimers;
-    }
-
-
-
-    function findCallback(callback, context) {
-      var result;
-      Object.keys(everytimers).forEach(function (id) {
-        var everytimer = everytimers[id];
-        if (callback === everytimer.callback) {
-          if (context === everytimer.context || everytimer.context === global_context) {
-            result = everytimer;
-          }
-        }
-      });
-      return result;
-    }
-
-
-
-    self.hasCallback = function () {
-      return !!findCallback.apply(self, arguments);
-    };
-
-
-
-    self.removeCallback = function(callback, context) {
-      var everytimer = findCallback(callback, context);
-      if (everytimer) {
-        delete everytimers[everytimer.id];
-        onetimers[everytimer.id] = undefined;
-        delete onetimers[everytimer.id];
+      self._index += 1;
+      if (self._index >= MAX_INT) {
+        self._cleanup(); // Works even for long-running processes
       }
 
       return self;
     };
 
-
-
-    self.deliver = function() {
-      if (fulfilled) {
-        options.error(new Error("`Future().fulfill(err, data, ...)` renders future deliveries useless"));
+    self.deliver = function () {
+      if (self.fulfilled) {
+        self._options.error(new Error("`Future().fulfill(err, data, ...)` renders future deliveries useless"));
         return;
       }
+
       var args = Array.prototype.slice.call(arguments);
-      data = args;
+      self._data = args;
 
-      deliveries += 1; // Eventually reaches `Infinity`...
+      self._deliveries += 1; // Eventually reaches `Infinity`...
 
-      Object.keys(everytimers).forEach(function (id) {
-        var everytimer = everytimers[id],
+      Object.keys(self._everytimers).forEach(function (id) {
+        var everytimer = self._everytimers[id],
           callback = everytimer.callback,
           context = everytimer.context;
 
-        if (onetimers[id]) {
-          delete everytimers[id];
-          delete onetimers[id];
+        if (self._onetimers[id]) {
+          delete self._everytimers[id];
+          delete self._onetimers[id];
         }
 
         // TODO
@@ -197,102 +159,159 @@
       });
 
       if (args[0] && "FutureTimeout" !== args[0].name) {
-        resetTimeout();
+        self._resetTimeout();
       }
+
 
       return self;
     };
+  }
 
+  Future.prototype.setContext = function (context) {
+    var self = this
+      ;
 
+    self._global_context = context;
+  };
 
-    self.fulfill = function () {
-      if (arguments.length) {
-        self.deliver.apply(self, arguments);
-      } else {
-        self.deliver();
-      }
-      fulfilled = true;
-    };
+  Future.prototype.setTimeout = function (new_time) {
+    var self = this
+      ;
 
+    self._time = new_time;
+    self._resetTimeout();
+  };
 
+  Future.prototype._resetTimeout = function () {
+    var self = this
+      ;
 
-    self.whenever = function (callback, local_context) {
-      var id = index,
-        everytimer;
-
-      if ('function' !== typeof callback) {
-        options.error(new Error("Future().whenever(callback, [context]): callback must be a function."));
-        return;
-      }
-
-      if (findCallback(callback, local_context)) {
-        // TODO log
-        options.error(new Error("Future().everytimers is a strict set. Cannot add already subscribed `callback, [context]`."));
-        return;
-      }
-
-      everytimer = everytimers[id] = {
-        id: id,
-        callback: callback,
-        context: (null === local_context) ? null : (local_context || global_context)
-      };
-
-      if (asap && deliveries > 0) {
-        // doesn't raise deliver count on purpose
-        everytimer.callback.apply(everytimer.context, data);
-        if (onetimers[id]) {
-          delete onetimers[id];
-          delete everytimers[id];
-        }
-      }
-
-      index += 1;
-      if (index >= MAX_INT) {
-        cleanup(); // Works even for long-running processes
-      }
-
-      return self;
-    };
-
-
-
-    self.when = function (callback, local_context) {
-      // this index will be the id of the everytimer
-      onetimers[index] = true;
-      self.whenever(callback, local_context);
-
-      return self;
-    };
-
-
-    //
-    function privatize(obj, pubs) {
-      var result = {};
-      pubs.forEach(function (pub) {
-        result[pub] = function () {
-          obj[pub].apply(obj, arguments);
-          return result;
-        };
-      });
-      return result;
+    if (self._timeout_id) {
+      clearTimeout(self._timeout_id);
+      self._timeout_id = undefined;
     }
 
-    passenger = privatize(self, [
+    if (self._time > 0) {
+      self._timeout_id = setTimeout(function () {
+        self.deliver(new FutureTimeoutException(self._time));
+        self._timeout_id = undefined;
+      }, self._time);
+    }
+  };
+
+  Future.prototype.callbackCount = function() {
+    var self = this
+      ;
+
+    return Object.keys(self._everytimers).length;
+  };
+
+  Future.prototype.deliveryCount = function() {
+    var self = this
+      ;
+
+    return self._deliveries;
+  };
+
+  Future.prototype.setAsap = function(new_asap) {
+    var self = this
+      ;
+
+    if (undefined === new_asap) {
+      new_asap = true;
+    }
+
+    if (true !== new_asap && false !== new_asap) {
+      self._options.error(new Error("Future.setAsap(asap) accepts literal true or false, not " + new_asap));
+      return;
+    }
+
+    self._asap = new_asap;
+  };
+
+  Future.prototype._findCallback = function (callback, context) {
+    var self = this
+      , result
+      ;
+
+    Object.keys(self._everytimers).forEach(function (id) {
+      var everytimer = self._everytimers[id]
+        ;
+
+      if (callback === everytimer.callback) {
+        if (context === everytimer.context || everytimer.context === self._global_context) {
+          result = everytimer;
+        }
+      }
+    });
+
+    return result;
+  };
+
+  Future.prototype.hasCallback = function () {
+    var self = this
+      ;
+
+    return !!self._findCallback.apply(self, arguments);
+  };
+
+  Future.prototype.removeCallback = function(callback, context) {
+    var self = this
+      , everytimer = self._findCallback(callback, context)
+      ;
+      
+    if (everytimer) {
+      delete self._everytimers[everytimer.id];
+      self._onetimers[everytimer.id] = undefined;
+      delete self._onetimers[everytimer.id];
+    }
+
+    return self;
+  };
+
+  Future.prototype.passable = function () {
+    var self = this
+      ;
+
+    self._passenger = privatize(self, [
       "when",
       "whenever"
     ]);
 
-    self.passable = function () {
-      return passenger;
-    };
+    return self._passenger;
+  };
 
+  // this will probably never get called and, hence, is not yet well tested
+  Future.prototype._cleanup = function () {
+    var self = this
+      , new_everytimers = {}
+      , new_onetimers = {}
+      ;
+
+    self._index = 0;
+    Object.keys(self._everytimers).forEach(function (id) {
+      var newtimer = new_everytimers[self._index] = self._everytimers[id];
+
+      if (self._onetimers[id]) {
+        new_onetimers[self._index] = true;
+      }
+
+      newtimer.id = self._index;
+      self._index += 1;
+    });
+
+    self._onetimers = new_onetimers;
+    self._everytimers = new_everytimers;
+  };
+
+  function create(context, options) {
+    // TODO use prototype hack instead of new?
+    return new Future(context, options);
   }
 
-  function Future(context, options) {
-    // TODO use prototype instead of new
-    return (new future(context, options));
-  }
+  Future.prototype.isFuture = isFuture;
 
   Future.isFuture = isFuture;
+  Future.create = create;
   module.exports = Future;
 }());
