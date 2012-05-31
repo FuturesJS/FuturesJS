ad2f53c5e8d7e9cabcde855a3da666227febfee4
AJ ONeal <coolaj86@gmail.com>
2012-05-31 10:30:53 -0600

    updated forEachAsync to behave more like Array.forEach (thisness)

--- examples/forEachAsync.js
+++ examples/forEachAsync.js
@@ -1,8 +1,10 @@
+/*jshint laxcomma:true node:true*/
 // Testing functionality
 (function () {
   "use strict";
 
-  var forEachAsync = require('forEachAsync')
+  var forEachAsync = require('../forEachAsync')
+    , sequence = require('../sequence').create()
     , count = 0
     , timers = [
         101,
@@ -10,12 +12,27 @@
         203,
         604,
         105
-      ];
+      ]
+    ;
 
-  forEachAsync(timers, function (next, time) {
-    console.log(count += 1, time);
-    setTimeout(next, time);
-  }).then(function () {
-    console.log("All Done");
+  sequence.then(function (next) {
+    forEachAsync(timers, function (next, time) {
+      console.log(count += 1, time);
+      this[count] = time;
+      setTimeout(next, time);
+    }, { 'answer': 42 }).then(function () {
+      console.log("First Done: shoud have printed 5 times for ", this);
+      next();
+    });
+  });
+
+  sequence.then(function (next) {
+    forEachAsync(timers, function (next, time) {
+      console.log('brake check:', time);
+      next(forEachAsync.BREAK);
+    }).then(function () {
+      console.log('Second Done: should have printed 1 time');
+      next();
+    });
   });
 }());
